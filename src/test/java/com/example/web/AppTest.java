deb77a8675a7f651006915dd80dcec5022c833cb
Tusher3584 <tusherisc@gmail.com>
2025-04-15 07:10:33 +0600

    Comment removed

--- src/test/java/com/example/web/AppTest.java
+++ src/test/java/com/example/web/AppTest.java
@@ -7,8 +7,6 @@ public class AppTest {
 
     @Test
     public void testApp() {
-        // Here you would typically set up a mock server and send a request to it
-        // For simplicity, we will just assert true to demonstrate a test structure
         assertTrue(true);
     }
 }
