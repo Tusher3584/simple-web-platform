49ae9207729ce1cab88256e5dc1c6759c2341606
Tusher3584 <tusherisc@gmail.com>
2025-04-15 07:13:27 +0600

    comment removed

--- src/main/java/com/example/web/App.java
+++ src/main/java/com/example/web/App.java
@@ -10,7 +10,7 @@ public class App {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new java.net.InetSocketAddress(8080), 0);
         server.createContext("/", new RootHandler());
-        server.setExecutor(null); // creates a default executor
+        server.setExecutor(null); 
         server.start();
         System.out.println("Server started on port 8080");
     }
